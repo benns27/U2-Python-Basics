0d03b95ab4ec50d0cdcc0b862e7a0292a80c2151
gsprint <sprint@gonzaga.edu>
2020-09-08 16:22:44 -0700

    Python Basics Fun from VQ1

---
+++ Python Basics Fun/hello_world.py
@@ -0,0 +1,9 @@
+# python executes code from top to bottom in this file
+result = (100 - 92) * 5
+print(result)
+
+ide = "VS Code"
+print(ide)
+print(ide.upper())
+
+# this is a one line comment. anything after the # on this line will be ignored by Python lkdjflaksdjfg;laj
